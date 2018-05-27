7144a5d2bcccf2b76e9e73c976d138e903314b44
yuyi <297341015@qq.com>
2018-05-27 22:02:44 +0800

    develop

---
+++ consul/kv_entity_test.go
@@ -0,0 +1,37 @@
+package consul
+
+import (
+	"testing"
+	"github.com/hashicorp/consul/api"
+)
+func TestNewKvEntity(t *testing.T) {
+	config := api.DefaultConfig()
+	config.Address = "127.0.0.1:8500"
+
+	client, _ := api.NewClient(config)
+	kv := NewKv(client.KV())
+	e := NewKvEntity(kv, "a", []byte("a"))
+	_, err := e.Set()
+
+	if err != nil {
+		t.Errorf("set error")
+	}
+
+	_, err = e.Get()
+
+	if err != nil {
+		t.Errorf("get error")
+	}
+
+	_, err = e.Delete()
+
+	if err != nil {
+		t.Errorf("delete error")
+	}
+
+	_, err = e.Get()
+
+	if err == nil {
+		t.Errorf("get error")
+	}
+}
