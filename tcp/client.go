412dd0b04a53e60b49ac43d757b04a5b14d8ac5b
huangxiaoan <huangxiaoan@xunlei.com>
2018-05-29 16:15:56 +0800

    develop

--- tcp/client.go
+++ tcp/client.go
@@ -11,13 +11,17 @@ import (
 	"sync/atomic"
 )
 
-const asyncWriteChanLen = 10000
-var NotConnect          = errors.New("not connect")
-var IsConnected         = errors.New("is connected")
-
+var (
+	NotConnect   = errors.New("not connect")
+ 	IsConnected  = errors.New("is connected")
+ 	WaitTimeout  = errors.New("wait timeout")
+ 	ChanIsClosed = errors.New("wait is closed")
+ 	UnknownError = errors.New("unknown error")
+)
 const (
 	statusConnect = 1 << iota
 )
+const asyncWriteChanLen = 10000
 
 type Client struct {
 	ctx               context.Context
@@ -35,26 +39,39 @@ type Client struct {
 	onConnect         OnConnectFunc
 	msgId             int64
 
-	waiter            map[int64]*Waiter
-	addWaiter         chan *Waiter
+	waiter            map[int64] *waiter
+	addwaiter         chan *waiter
+	resChan           chan *res
+	delwaiter         chan int64
 }
 
-type Waiter struct {
+type waiter struct {
 	MsgId int64
-	Data chan []byte
+	Data chan *waiterData
 	Time int64
 }
-var WaitTimeout = errors.New("wait timeout")
-var ChanIsClosed = errors.New("wait is closed")
-var UnknownError = errors.New("unknown error")
-func (w *Waiter) Wait(timeout time.Duration) ([]byte, error) {
+
+type waiterData struct {
+	client *Client
+	data []byte
+	msgId int64
+}
+
+type res struct {
+	MsgId int64
+	Data []byte
+}
+
+
+func (w *waiter) Wait(timeout time.Duration) ([]byte, error) {
 	a := time.After(timeout)
 	select {
 	case data ,ok := <- w.Data:
 		if !ok {
 			return nil, ChanIsClosed
 		}
-		return data, nil
+		data.client.delwaiter <- data.msgId
+		return data.data, nil
 	case <- a:
 		return nil, WaitTimeout
 	}
@@ -107,8 +124,10 @@ func NewClient(ctx context.Context, ip string, port int, opts ...ClientOption) *
 		coder:             &Codec{},
 		bufferSize:        4096,
 		msgId:             0,
-		waiter:            make(map[int64]*Waiter),
-		addWaiter:         make(chan *Waiter, 10000),
+		waiter:            make(map[int64]*waiter),
+		addwaiter:         make(chan *waiter, 10000),
+		resChan:           make(chan *res, 10000),
+		delwaiter:         make(chan int64, 10000),
 	}
 	for _, f := range opts {
 		f(c)
@@ -129,7 +148,7 @@ func (tcp *Client) AsyncWrite(data []byte) {
 	tcp.asyncWriteChan <- data
 }
 
-func (tcp *Client) Send(data []byte) (*Waiter, error) {
+func (tcp *Client) Send(data []byte) (*waiter, error) {
 	if tcp.status & statusConnect <= 0 {
 		return nil, NotConnect
 	}
@@ -139,12 +158,12 @@ func (tcp *Client) Send(data []byte) (*Waiter, error) {
 	if err != nil {
 		return nil, err
 	}
-	wai := &Waiter{
+	wai := &waiter{
 		MsgId: msgId,
-		Data:  make(chan []byte, 1),
+		Data:  make(chan *waiterData, 1),
 		Time:  int64(time.Now().UnixNano() / 1000000),
 	}
-	tcp.addWaiter <- wai
+	tcp.addwaiter <- wai
 	return wai, nil
 }
 
@@ -161,6 +180,13 @@ func (tcp *Client) keep() {
 		case <- c :
 			// keepalive
 			tcp.Send([]byte(""))
+			for msgId, v := range tcp.waiter  {
+				// check timeout
+				if int64(time.Now().UnixNano() / 1000000) - v.Time >= 6000 {
+					close(v.Data)
+					delete(tcp.waiter, msgId)
+				}
+			}
 		case sendData, ok := <- tcp.asyncWriteChan:
 			//async send support
 			if !ok {
@@ -170,11 +196,30 @@ func (tcp *Client) keep() {
 			if err != nil {
 				log.Errorf("send failure: %+v", err)
 			}
-			//if n < len(sendData) {
-			//	log.Errorf("send not complete")
-			//}
-		case wai := <- tcp.addWaiter:
+			// new send
+		case wai, ok := <- tcp.addwaiter:
+			if !ok {
+				return
+			}
 			tcp.waiter[wai.MsgId] = wai
+			// server reply, write data to channel
+		case res, ok := <- tcp.resChan:
+			if !ok {
+				return
+			}
+			w, ok := tcp.waiter[res.MsgId]
+			if ok {
+				w.Data <- &waiterData{tcp, res.Data, res.MsgId}
+			}
+		case msgId, ok := <- tcp.delwaiter:
+			if !ok {
+				return
+			}
+			w, ok:=tcp.waiter[msgId]
+			if ok {
+				close(w.Data)
+				delete(tcp.waiter, msgId)
+			}
 		}
 	}
 }
@@ -256,7 +301,7 @@ func (tcp *Client) onMessage(msg []byte) {
 	tcp.buffer = append(tcp.buffer, msg...)
 	for {
 		olen := len(tcp.buffer)
-		_, content, pos, err := tcp.coder.Decode(tcp.buffer)
+		msgId, content, pos, err := tcp.coder.Decode(tcp.buffer)
 		if err != nil {
 			log.Errorf("%v", err)
 			tcp.buffer = make([]byte, 0)
@@ -268,6 +313,7 @@ func (tcp *Client) onMessage(msg []byte) {
 			tcp.buffer = make([]byte, 0)
 			log.Errorf("pos %v (olen=%v) error, content=%v(%v) len is %v, data is: %+v", pos, olen, content, string(content), len(tcp.buffer), tcp.buffer)
 		}
+		tcp.resChan <- &res{MsgId:msgId, Data:content}
 		for _, f := range tcp.onMessageCallback {
 			f(tcp, content)
 		}
