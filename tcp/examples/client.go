145229fd7e8afc0100f193f1e6f79d65f20d7c4a
yuyi <297341015@qq.com>
2018-05-29 09:04:34 +0800

    tcp debug

--- tcp/examples/client.go
+++ tcp/examples/client.go
@@ -27,7 +27,7 @@ func main() {
 			80, tcp.SetOnMessage(func(tcp *tcp.Client, content []byte) {
 			fmt.Println(string(content))
 		}), tcp.SetOnConnect(func(tcp *tcp.Client) {
-			d := "GET / HTTP/1.1\r\nAccept: */*"
+			d := "GET / HTTP/1.1\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36\r\nHost: www.baidu.com\r\nAccept-Language:zh-cn\r\n\r\nhello"
 			n, e:= tcp.Write([]byte(d))
 			fmt.Println(n, e)
 		}))
