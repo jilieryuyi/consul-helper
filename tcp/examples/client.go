21e251561b200b02c264361333f0458d92b2eb47
yuyi <297341015@qq.com>
2018-05-31 07:03:02 +0800

    optimize

--- tcp/examples/client.go
+++ tcp/examples/client.go
@@ -7,7 +7,10 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"bytes"
+	"net/http"
+	_ "net/http/pprof"
 )
+
 func main() {
 	// 先运行server端
 	// go run server.go
@@ -22,6 +25,22 @@ func main() {
 		return
 	}
 	defer client.Disconnect()
+
+	go func() {
+		//http://localhost:8880/debug/pprof/  内存性能分析工具
+		//go tool pprof logDemo.exe --text a.prof
+		//go tool pprof your-executable-name profile-filename
+		//go tool pprof your-executable-name http://localhost:8880/debug/pprof/heap
+		//go tool pprof wing-binlog-go http://localhost:8880/debug/pprof/heap
+		//https://lrita.github.io/2017/05/26/golang-memory-pprof/
+		//然后执行 text
+		//go tool pprof -alloc_space http://127.0.0.1:8880/debug/pprof/heap
+		//top20 -cum
+
+		//下载文件 http://localhost:8880/debug/pprof/profile
+		//分析 go tool pprof -web /Users/yuyi/Downloads/profile
+		http.ListenAndServe("127.0.0.1:8881", nil)
+	}()
 
 	start := time.Now()
 	times := 1000000
